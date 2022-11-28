88953fcde7d91c8d7e0b882f4f98fc439d3f80d2
abraaoribeiro23 <abraaoribeiroo23@gmail.com>
2022-11-28 12:48:45 -0300

    feat: RandomNumberToBase64String

---
+++ RandomNumberToBase64String/RandomNumberToBase64String.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using Xunit;
+
+namespace RandomNumberToBase64String
+{
+    public class RandomNumberToBase64String
+    {
+        [Fact]
+        public void RandomNumberToBase64StringTest()
+        {
+            using var provider = RandomNumberGenerator.Create();
+            var bytes = new byte[32];
+            provider.GetBytes(bytes);
+
+            var resultToken = Convert.ToBase64String(bytes);
+            Assert.NotEmpty(resultToken);
+        }
+    }
+}
